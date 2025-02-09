edf4241e15139d77524970430e22b7df2f584504
Aritree Hoque <aritree.hoque@gmail.com>
2025-02-08 22:20:14 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -15,3 +15,16 @@ const calculateSalesTax = function(amount, taxRate) {
 // Test Data
 calculateSalesTax(100, 0.07);
 calculateSalesTax(500, 0.1);
+
+// Task3- Arrow Function
+const calculateBonus = (salary, performanceRating) => {
+    let bonusRate = 0;
+    if (performanceRating === "Excellent") bonusRate = 0.2; // 20% of salary
+    else if (performanceRating === "Good") bonusRate = 0.1; // 10% of salary
+    else if (performanceRating === "Average") bonusRate = 0.05; // 5% of salary
+    let bonus = salary * bonusRate;
+    console.log(`Bonus: $${bonus}`);
+}; // Determining a bonus.
+// Test Data
+calculateBonus(5000, "Excellent");
+calculateBonus(7000, "Good");
